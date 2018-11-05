68a47d0e9839c31082de5add29485a09c30591bc
Mohamed Benkedadra <hammicristo@gmail.com>
2018-11-05 23:40:58 +0100

    added cookies

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const url = require('url');
 var fs = require('fs');
 
 // require('electron-reload')(__dirname, { electron: require(  path.join(__dirname, 'node_modules', 'electron') ) });
-require('electron-reload')(__dirname);
+// require('electron-reload')(__dirname);
 
 const {app, BrowserWindow, Menu, session, ipcMain, webContents} = electron;
 var jsonFile = require('./accountInfo'); 
 let addAccountWindow;
 let account = [];
 let accountList = {};
-
+const ea_site = "https://fantasy.premierleague.com/";
 
 app.on('ready', function(){
 	// Create new window
@@ -52,60 +52,28 @@ function createAddAccountWindow(){
 }
 
 // ea web app
-function createEaWindow(emailAcc, passAcc){
-	// session.defaultSession.clearStorageData();
-	// var loadingWindow = new BrowserWindow({width: 600, height: 400, transparent: true, frame: false, parent: mainWindow, modal: true});
-	// add loadingWindow 
-	// loadingWindow.loadURL(url.format({
-	// 	pathname: path.join(__dirname, 'views/loading.html'),
-	// 	protocol: 'file:',
-	// 	slashes:true
-	// }));
-	// create ea window
+function activate_account(email){
+	accountList[email].window = new BrowserWindow({width:600, height:400, show:false, parent: mainWindow});
+	accountList[email].window.webContents.session.clearStorageData({storages:['cookies']});
 
-	// if (accountList[emailAcc].window) {
-	// 	accountList[emailAcc].window.show();
-	// 	console.log("here")
-	// 	return;
-	// }
-
-	accountList[emailAcc].window = new BrowserWindow({width:600, height:400, show:false, parent: mainWindow});
-	accountList[emailAcc].window.webContents.session.clearStorageData();
-	// if (accountList[emailAcc].cookies)
-	// 	accountList[emailAcc].cookies.forEach(cookie => {
-	// 		accountList[emailAcc].window.webContents.session.cookies.set( cookie, () => {} );
-	// 	});
-	// 	accountList[emailAcc].cookies.forEach(cookie => {
-	// 		session.defaultSession.cookies.set( cookie, () => {} );
-	// 	});
-
-
-	accountList[emailAcc].window.on('close', event => {
-		event.preventDefault();
-		accountList[emailAcc].window.hide();
-		// accountList[emailAcc].window.webContents.session.cookies.get({}, (error, cookies) => {
-		// 	accountList[emailAcc].cookies = cookies;
-		// 	console.log(accountList[emailAcc].cookies);
-		// });
+	accountList[email].cookies.forEach(cookie => {
+		cookie.url = ea_site;
+		accountList[email].window.webContents.session.cookies.set( cookie, error => {
+			if (error)
+				console.dir(error);
+		});
 	});
 
+	accountList[email].window.on('close', () => {
 
-
-	accountList[emailAcc].window.loadURL('https://fantasy.premierleague.com/');
-
-	// send email and pass to ea 
-	accountList[emailAcc].window.webContents.on('did-finish-load', () => {
-		
-		accountList[emailAcc].window.webContents.executeJavaScript(
-			`document.getElementById("ismjs-username").value = '${emailAcc}';`
-		);
-		accountList[emailAcc].window.webContents.executeJavaScript(
-			`document.getElementById("ismjs-password").value = '${passAcc}';`
-		);
-		// loadingWindow.destroy();
-		accountList[emailAcc].window.show();
-		
+		accountList[email].window = null;
 	});
+
+	accountList[email].window.loadURL(ea_site);
+
+	// accountList[email].window.webContents.on('did-finish-load', () => {
+	// 	accountList[email].window.show();
+	// });
 }
 
 
@@ -143,25 +111,60 @@ if(process.env.NODE_ENV !== 'production'){
 
 // Attach listener in the main process with the given ID
 ipcMain.on('requestHandler', (event, data) => {
-	if(data.type === 'saveAccount'){
-		//catch email-acc and password-acc
-		addAccountWindow.close();
-		accountList[data.email] = {
-			password : data.password,
-			// cookies: null,
-			window: null,
-		};
-		save_account_data();
-	}
+	if(data.type === 'saveAccount')
+		add_new_account(data);
 
 	if(data.type === 'addAccount')
 		createAddAccountWindow();
 
 	if(data.type === 'openEaWindow')
-		createEaWindow(data.email,accountList[data.email].password);
+		activate_account(data.email);
 
 });
 
+function add_new_account(data) {
+	var window = new BrowserWindow({width:600, height:400, show:false, parent: mainWindow});
+	window.webContents.session.clearStorageData({storages:['cookies']});
+
+	window.on('close', event => {
+		accountList[data.email] = {
+			password : data.password
+		};
+
+		window.webContents.session.cookies.get({}, (error, retrieved_cookies) => {
+			accountList[data.email].cookies = retrieved_cookies;
+			save_account_data();
+		});
+
+		addAccountWindow.close();
+
+		add_account_to_menu(data.email);
+
+	});
+
+	window.loadURL(ea_site);
+
+	// send email and pass to ea 
+	window.webContents.on('did-finish-load', () => {
+		
+		window.webContents.executeJavaScript(
+			`document.getElementById("ismjs-username").value = '${data.email}';`
+		);
+		window.webContents.executeJavaScript(
+			`document.getElementById("ismjs-password").value = '${data.password}';`
+		);
+
+		window.show();
+		
+	});
+}
+
+
+function add_account_to_menu(email) {
+	mainWindow.webContents.executeJavaScript(`
+		document.getElementById('accounts-list').insertAdjacentHTML('beforeend','<div class= "email"><p>${ email }</p><span class="switch"><input class="check" type="checkbox" onclick="isChecked(this)"><span class="slider"></span></span></div>');
+	`);
+}
 
 function save_account_data() {
 	for (let email in accountList) {
--- views/dala3.js
+++ views/dala3.js
@@ -1,78 +1,43 @@
 const {ipcRenderer} = require('electron');
 
+function fill_menu() {
+    // get account data from json file
+    var getAccounts = new XMLHttpRequest();
+    getAccounts.open('GET','../accountInfo.json');
+    getAccounts.onload = () => {
+        var textHtml = '';
+        var xdata = JSON.parse(getAccounts.responseText);
 
-// get account data from json file
-var getAccounts = new XMLHttpRequest();
-console.log(getAccounts);
-getAccounts.open('GET','../accountInfo.json');
-getAccounts.onload = function(){
-    var textHtml = ''
-    var xdata = JSON.parse(getAccounts.responseText);
-    var parent = document.getElementById('parent');
-    for(i=0;i<Object.keys(xdata[0]).length;i++){
-        textHtml += '<div class= "email">'+'<p>'+ Object.keys(xdata[0])[i] + '</p>' + '<label class="switch">' + '<input class="check" type="checkbox" onclick="isChecked(this)">'+'<span class="slider">'+'</span>'+'</label>'+'</div>';
-        console.log(Object.keys(xdata[0])[i]);
-    }
-    parent.insertAdjacentHTML("beforeend",textHtml);
-   
-};
-getAccounts.send();
+        for(i = 0; i < Object.keys(xdata[0]).length; i++)
+            textHtml += `<div class= "email"><p>${ Object.keys(xdata[0])[i] }</p><span class="switch"><input class="check" type="checkbox" onclick="isChecked(this)"><span class="slider"></span></span></div>`;
 
-// send add account if click
-var add = document.querySelector("#add-account");
+        document.getElementById('accounts-list').insertAdjacentHTML("beforeend", textHtml);
+    };
+    getAccounts.send();
+}
 
 
-add.addEventListener('click', ()=> {
-    
-    let data = {
-            
-        type: 'addAccount'
-    }
-    ipcRenderer.send('requestHandler', data);
-
-});
+function add_account() {
+    // send add account if click
+    document.querySelector("#add-account").addEventListener('click', ()=> {
+        let data = { type: 'addAccount' };
+        ipcRenderer.send('requestHandler', data);
+    });
+}
 
 
-
-// open ea window when checkebox is checked and send email of checkbox target
-
-var infoemail = document.getElementsByTagName("p");
-console.log(infoemail)
-var ch = document.getElementsByClassName("check");
-function isChecked(el){
-    
-    if(el.checked === true){
-        
+function isChecked(elem){
+    if(elem.checked === true){
         let data = {
-            email: infoemail[indexInClass(el)+2].innerText,
+            email: elem.parentElement.parentElement.querySelector('p').innerText,
             type: 'openEaWindow'
-        }
-        ;
+        };
         ipcRenderer.send('requestHandler', data);
     }
-    
 }
-//get class index of checkbox
-function indexInClass(node) {
-    var className = node.className;
-    var num = 0;
-    for (var i = 0; i < ch.length; i++) {
-      if (ch[i] === node) {
-        return num;
-      }
-      num++;
-    }
-    return -1;
-  }
 
 
-
-
-
-
-
-
-        
-
-    
-        
+document.addEventListener('DOMContentLoaded', () => {
+    fill_menu();
+    add_account();
+});
